1f1461bf2d80944dc96cbeaa69db016642994193
María Bosom <100429055@alumnos.uc3m.es>
2021-10-19 14:49:50 +0200

    Cosas de clase

--- script/popups.js
+++ script/popups.js
@@ -1,7 +1,25 @@
 function openForm() {
-  document.getElementById("login").style.display = "block";
+  document.getElementById("login").style.display = "flex";
 }
 
 function closeForm() {
   document.getElementById("login").style.display = "none";
 }
+
+var e = new Event("look", {"cancelable":true});
+$("#submit-log").click(function(e) {
+  e.preventDefault();
+  var username = document.getElementById('username').value;
+  var pwd = document.getElementById('pwd').value;
+
+  if ()
+
+})
+
+$("#submit-sign").click(function(e) {
+  e.preventDefault():
+  var username = document.getElementById('username').value;
+  var pwd = document.getElementById('pwd').value;
+
+  document.cookie = username + "=" + pwd;
+})
